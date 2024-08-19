72e3bbe4a5589043618871b8788bb6e4e008124e
Aliaksei Tsitovich <AliakseiT@users.noreply.github.com>
2024-08-19 22:38:37 +0200

    Add logging and wrap string parsing in try catch

--- lib/src/utils/datetime_utils.dart
+++ lib/src/utils/datetime_utils.dart
@@ -1,19 +1,26 @@
+import 'dart:developer' as developer;
+
 class DateTimeUtils {
   static Duration? parseDuration(dynamic offset) {
     if (offset == null) return null;
     if (offset is int) {
       return Duration(hours: offset);
     } else if (offset is String) {
-      final regex = RegExp(r'^([+-])(\d{2}):(\d{2})$');
-      final match = regex.firstMatch(offset);
-      if (match == null) return null;
+      try {
+        final regex = RegExp(r'^([+-])(\d{2}):(\d{2})$');
+        final match = regex.firstMatch(offset);
+        if (match == null) return null;
 
-      final sign = match.group(1) == '+' ? 1 : -1;
-      final hours = int.parse(match.group(2)!);
-      final minutes = int.parse(match.group(3)!);
+        final sign = match.group(1) == '+' ? 1 : -1;
+        final hours = int.parse(match.group(2)!);
+        final minutes = int.parse(match.group(3)!);
 
-      return Duration(hours: hours * sign, minutes: minutes * sign);
+        return Duration(hours: hours * sign, minutes: minutes * sign);
+      } on Exception catch (e) {
+        developer.log(e.toString(), level: 900);
+      }
     }
+    developer.log('Cannot parse offset $offset', level: 900);
     return null;
   }
 }
